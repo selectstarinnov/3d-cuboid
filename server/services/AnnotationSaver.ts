04d61d0462407fb56c2fce81518677302d8001b3
ijunc2 <ijunc2@gmail.com>
2024-02-06 13:31:13 +0900

    * annotation json 파일은 기존 세팅이 없어도 (초기생성) 저장 및 작동 가능하도록 수정 * node 폴더 분리 / 서비스별

--- server/services/AnnotationSaver.ts
+++ server/services/AnnotationSaver.ts
@@ -1,24 +1,10 @@
 const fs = require('fs');
 const requirePath = require("../requirePath.ts")
-const {dirname, pad} = requirePath("server/utils.ts")
-const classes = requirePath("config/classes.js");
-
-
-const initAnnotationFile = (index) => ({
-// {"name":"000001","timestamp":0,"index":1,"labels":[]}
-    name: pad(index, 6),
-    timestamp: 0,
-    index,
-    labels: []
-})
+const {dirname} = requirePath("server/utils.ts")
 
 const annotationSaver = (req, res) => {
     const fileBody = req.body;
     const filePath = dirname + '/' + fileBody.path;
-    if(!fs.existsSync(filePath)){
-        const initData = initAnnotationFile(fileBody.index);
-        fs.writeFileSync(`${initData.name}.json`, JSON.stringify(initData, null, 4), 'utf8');
-    }
     delete fileBody['path']
     fs.writeFileSync(filePath, JSON.stringify(fileBody))
     res.send({
